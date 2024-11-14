41dc0c60625fbe5a72d6baf608861b24162c8d31
Ryan.Webber <rweb217@wgu.edu>
2024-11-14 11:45:17 -0800

    Initial Commit

---
+++ src/test/java/com/d288/ryan/RyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.ryan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
